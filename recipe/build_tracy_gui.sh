af7f44b2b5154d0d09d98f3c2e8aacb2a5635201
Joris Vaillant <joris.vaillant@inria.fr>
2025-11-13 17:47:28 +0100

    Try to fix osx build

--- recipe/build_tracy_gui.sh
+++ recipe/build_tracy_gui.sh
@@ -9,9 +9,13 @@ mkdir build && cd build
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
+    # pugixml use CXX 20 module and need clang-scan-deps.
+    # It's installed by the clang-tools package, but it doesn't have the right CMAKE_TOOLCHAIN_PREFIX.
+    CMAKE_CUSTOM_ARGS="-DCMAKE_CXX_COMPILER_CLANG_SCAN_DEPS="clang-scan-deps""
 fi
 
-cmake ${CMAKE_ARGS} .. \
+cmake ${CMAKE_ARGS} ${CMAKE_CUSTOM_ARGS} .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DLEGACY=TRUE \
     -DDOWNLOAD_CAPSTONE=FALSE \
