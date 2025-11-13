b3143bff4d2a464c3e9d6249a2c03c0dbae6ebf9
Joris Vaillant <joris.vaillant@inria.fr>
2025-11-13 21:44:43 +0100

    Fix binding CXX 20 build

--- recipe/build_tracy_client_python.sh
+++ recipe/build_tracy_client_python.sh
@@ -3,5 +3,10 @@
 rm -rf build
 cd python
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # Binding setup CXX 20 and need clang-scan-deps.
+    # It's installed by the clang-tools package, but it doesn't have the right CMAKE_TOOLCHAIN_PREFIX.
+    export SKBUILD_CMAKE_DEFINE=CMAKE_CXX_COMPILER_CLANG_SCAN_DEPS=clang-scan-deps
+fi
 export CMAKE_GENERATOR=Ninja
 ${PYTHON} -m pip install --no-deps --no-build-isolation -vv .
