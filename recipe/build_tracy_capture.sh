cdbed457bb2425440cec1073cfc64e15fc0647e3
Bas Zalmstra <4995967+baszalmstra@users.noreply.github.com>
2026-03-26 14:30:37 +0100

    Add LEGACY=TRUE to CLI builds to avoid C++20 module scanning on macOS

--- recipe/build_tracy_capture.sh
+++ recipe/build_tracy_capture.sh
@@ -7,15 +7,11 @@ mkdir build && cd build
 
 if [[ "${target_platform}" == osx-* ]]; then
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-
-    # pugixml use CXX 20 module and need clang-scan-deps.
-    # It's installed by the clang-tools package, but it doesn't have the right CMAKE_TOOLCHAIN_PREFIX.
-    CLANG_SCAN_DEPS=$(which clang-scan-deps)
-    CMAKE_CUSTOM_ARGS="-DCMAKE_CXX_COMPILER_CLANG_SCAN_DEPS=${CLANG_SCAN_DEPS}"
 fi
 
-cmake ${CMAKE_ARGS} ${CMAKE_CUSTOM_ARGS} .. \
+cmake ${CMAKE_ARGS} .. \
     -DCMAKE_BUILD_TYPE=Release \
+    -DLEGACY=TRUE \
     -DNO_ISA_EXTENSIONS=ON \
     -DDOWNLOAD_CAPSTONE=FALSE \
     -GNinja
